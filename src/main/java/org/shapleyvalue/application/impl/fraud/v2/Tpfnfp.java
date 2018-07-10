0ce1dffa455c91b39eccbf5b4a0162b95ef19b20
franck-benault <franck-benault@orange.fr>
2018-07-10 23:20:07 +0200

    [APPLI] complete tpfpfn

--- src/main/java/org/shapleyvalue/application/impl/fraud/v2/Tpfnfp.java
+++ src/main/java/org/shapleyvalue/application/impl/fraud/v2/Tpfnfp.java
@@ -1,10 +1,40 @@
 package org.shapleyvalue.application.impl.fraud.v2;
 
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Tpfnfp {
+	
+	private final Logger logger = LoggerFactory.getLogger(Tpfnfp.class);
+
 	
 	private int truePositif;
 	private int falseNegatif;
 	private int falsePositif;
+	
+	public Tpfnfp(List<RuledTransaction> tx, List<Integer> rules) {
+
+		for(RuledTransaction t : tx) {
+			boolean isFired = false;
+			for(Integer rule : rules) {
+				if(t.getRules().contains(rule)) isFired =true;
+			}
+			if(t.isFraud()) {
+				if(isFired) {
+					truePositif++;
+				} else {
+					falseNegatif++;
+				}
+			} else {
+				if(isFired) {
+					falsePositif++;
+				}
+			}
+		}
+		
+		logger.info("{} {} {} ", truePositif, falseNegatif, falsePositif);
+	}
 	
 	public Tpfnfp(int tp, int fn, int fp) {
 		truePositif = tp;
@@ -25,6 +55,8 @@ public class Tpfnfp {
 		
 		double precision = precision();
 		double recall = recall();
+		
+		if((precision + recall) ==0) return 0;
 		
 		return (precision*recall*2) / (precision + recall); 
 		
--- src/test/java/org/shapleyvalue/application/impl/fraud/v2/TpfnfpTest.java
+++ src/test/java/org/shapleyvalue/application/impl/fraud/v2/TpfnfpTest.java
@@ -2,6 +2,10 @@ package org.shapleyvalue.application.impl.fraud.v2;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 public class TpfnfpTest {
@@ -26,6 +30,36 @@ public class TpfnfpTest {
 	
 		assertEquals(v.score(), 0.5, 0.01);
 	}
+	
+	@Test
+	public void testScoreIs1() {
+		Tpfnfp v = new Tpfnfp(1,0,0);
+	
+		assertEquals(v.score(), 1.0, 0.01);
+	}
+	
+	
+	@Test
+	public void testScoreIs0() {
+		Tpfnfp v = new Tpfnfp(0,1,1);
+	
+		assertEquals(v.score(), 0.0, 0.01);
+	}
 
-
+	@Test
+	public void testScoreWithRules() {
+		RuledTransaction t1 = new RuledTransaction("1,1,0");
+		RuledTransaction t2 = new RuledTransaction("1,0,1");
+		
+		List<RuledTransaction> transactions = new ArrayList<>();
+		transactions.add(t1);
+		transactions.add(t2);
+		
+		Tpfnfp v1 = new Tpfnfp(transactions, Arrays.asList(1));
+		assertEquals(v1.score(), 0.66, 0.01);
+		
+		Tpfnfp v2 = new Tpfnfp(transactions, Arrays.asList(1));
+		assertEquals(v2.score(), 0.66, 0.01);
+		
+	}
 }
