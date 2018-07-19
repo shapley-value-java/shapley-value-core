43c7eceebcda31be8d94ac6718d1c6c7f028ac4a
franck-benault <franck-benault@orange.fr>
2018-07-19 23:41:33 +0200

    [FIX] fix for dummy user

--- src/main/java/org/shapleyvalue/application/impl/fraud/v2/Tpfnfp.java
+++ src/main/java/org/shapleyvalue/application/impl/fraud/v2/Tpfnfp.java
@@ -80,10 +80,14 @@ public class Tpfnfp {
 	
 	public double precision() {
 		
+		if(truePositif+falsePositif==0) return 0;
+		
 		return ((double)truePositif)/(truePositif+falsePositif);
 	}
 	
 	public double recall() {
+		if(truePositif+falseNegatif==0) return 0;
+		
 		return ((double) truePositif)/(truePositif+falseNegatif);
 	}
 	
